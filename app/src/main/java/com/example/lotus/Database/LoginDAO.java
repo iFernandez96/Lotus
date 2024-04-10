eb72bc4d8aa4058d7ad8697a17689aa7943be28a
Saul <sahuerta@csumb.edu>
2024-04-10 15:19:09 -0700

    Worked on LogIn Database

---
+++ app/src/main/java/com/example/lotus/Database/LoginDAO.java
@@ -0,0 +1,21 @@
+package com.example.lotus.Database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.lotus.Database.entities.Login;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Dao
+public interface LoginDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Login login);
+
+    @Query("Select * from " + LoginDatabase.LOGIN_TABLE)
+
+    ArrayList<Login> getAllRecords();
+}
--- app/src/main/java/com/example/lotus/MainActivity.java
+++ app/src/main/java/com/example/lotus/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "Lotus";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
