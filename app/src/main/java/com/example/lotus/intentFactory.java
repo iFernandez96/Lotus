5a0004f41bf1958a06733f2699c88a296555bc26
Israel Fernandez <israelfernandez96@gmail.com>
2024-04-17 10:54:14 -0700

    Added intentFactory class

---
+++ app/src/main/java/com/example/lotus/intentFactory.java
@@ -0,0 +1,46 @@
+package com.example.lotus;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class intentFactory {
+    public static Intent createIntent(Context context, Class<?> cls, String key, boolean value) {
+        Intent intent = new Intent(context, cls);
+        intent.putExtra(key, value);
+        return intent;
+    }
+
+    public static Intent createIntent(Context context, Class<?> cls, String key, char value) {
+        Intent intent = new Intent(context, cls);
+        intent.putExtra(key, value);
+        return intent;
+    }
+
+    public static Intent createIntent(Context context, Class<?> cls, String key, String value) {
+        Intent intent = new Intent(context, cls);
+        intent.putExtra(key, value);
+        return intent;
+    }
+
+    public static Intent createIntent(Context context, Class<?> cls, String key, int value) {
+        Intent intent = new Intent(context, cls);
+        intent.putExtra(key, value);
+        return intent;
+    }
+    public static Intent createIntent(Context context, Class<?> cls, String key, float value) {
+        Intent intent = new Intent(context, cls);
+        intent.putExtra(key, value);
+        return intent;
+    }
+    public static Intent createIntent(Context context, Class<?> cls, String key, double value) {
+        Intent intent = new Intent(context, cls);
+        intent.putExtra(key, value);
+        return intent;
+    }
+
+    public static Intent createIntent(Context context, Class<?> cls, String key, long value) {
+        Intent intent = new Intent(context, cls);
+        intent.putExtra(key, value);
+        return intent;
+    }
+}
