151b1ae52e075ca9295e496596038dbd1351bb64
Israel Fernandez <israelfernandez96@gmail.com>
2024-04-19 16:45:32 -0700

    Added persistent Login

--- app/src/main/java/com/example/lotus/MainActivity.java
+++ app/src/main/java/com/example/lotus/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lotus;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
 //    private static final String MAIN_ACTIVITY_KEY="MAIN_ACT";
     public static final String TAG = "Lotus";
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart(){
         super.onStart();
         EdgeToEdge.enable(this);
+        if (isUserLoggedIn(getApplicationContext())) {
+            Intent intent = intentFactory.createIntent(getApplicationContext(), LandingPage.class);
+            intent.putExtra(Constants.LOGIN_ACTIVITY_KEY, username);
+            startActivity(intent);
+            return;
+        }
         Intent intent = intentFactory.createIntent(getApplicationContext(), LoginActivity.class);
         startActivity(intent);
+    }
+    public boolean isUserLoggedIn(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("LoginPrefs", Context.MODE_PRIVATE);
+        username = prefs.getString("username", "");
+        return prefs.getBoolean("isLoggedIn", false);  // Default to false if the flag isn't found
     }
 }
