876e265fbcc1f01c9338b83ff04e090d9f7c6866
Saul <sahuerta@csumb.edu>
2024-04-30 18:05:13 -0700

    worked on notifications permissions, added png of lotus_button_start_head_tracking.png

---
+++ app/src/main/java/SettingsFragment.java
@@ -0,0 +1,14 @@
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.example.lotus.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+        setPreferencesFromResource(R.xml.settings_options, rootKey);
+    }
+}
