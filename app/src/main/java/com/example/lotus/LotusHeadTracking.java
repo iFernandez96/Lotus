3bf4caf108d14316e33cb3fea2bdf755b208995e
Israel Fernandez <israelfernandez96@gmail.com>
2024-05-02 15:21:48 -0700

    Refactored, and updated LandingPage for calling HeadTracking Also removed unary + from proof of concepts

--- app/src/main/java/com/example/lotus/LotusHeadTracking.java
+++ app/src/main/java/com/example/lotus/LotusHeadTracking.java
@@ -4,14 +4,19 @@ import android.content.Context;
 
 public class LotusHeadTracking {
     private final Context context; // Added to hold the Context
+    private LotusHeadTracking tracker;
 
     public LotusHeadTracking(Context context) { // Constructor to receive a Context
         this.context = context;
         Tracker tracker = new Tracker(context);
+        tracker.initializeSensors();
     }
 
+    public void startTracking() {
+        tracker.startTracking();
+    }
 
-
-
-
+    public void stopTracking() {
+        tracker.stopTracking();
+    }
 }
