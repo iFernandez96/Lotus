ac268d674568b18cae4e17b7e93c18b6a7b65fdd
Saul <sahuerta@csumb.edu>
2024-05-01 20:37:26 -0700

    worked on settings/ added sound, doesn't work yet

--- app/src/main/java/com/example/lotus/LandingPage.java
+++ app/src/main/java/com/example/lotus/LandingPage.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +20,7 @@ import com.example.lotus.databinding.ActivityLandingPageBinding;
 public class LandingPage extends AppCompatActivity {
     private LoginRepo repository;
     private String username;
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +36,12 @@ public class LandingPage extends AppCompatActivity {
             landingPageBinding.usernameView.setText(username);
             landingPageBinding.isAdmin.setVisibility(user.isAdmin() ? View.VISIBLE : View.GONE);
         }
+
+        /*
+
+        playSound(R.raw.autooff);
+
+         */
         Button settingsButton = landingPageBinding.settingsButton;
 
         settingsButton.setOnClickListener(new View.OnClickListener() {
@@ -57,5 +65,13 @@ public class LandingPage extends AppCompatActivity {
             finish();
         }
 
+    }
+
+    private void playSound(int soundResourceId){
+
+        mediaPlayer = MediaPlayer.create(this,soundResourceId);
+        mediaPlayer.start();
+
+        //mediaPlayer.setOnCompletionListener();
     }
 }
