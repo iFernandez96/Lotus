6ba4289b1f14e6d6d591327cb3b1b9afb4fc197b
Israel Fernandez <israelfernandez96@gmail.com>
2024-05-02 20:01:00 -0700

    Fixed

--- app/src/main/java/com/example/lotus/ProfileActivity.java
+++ app/src/main/java/com/example/lotus/ProfileActivity.java
@@ -1,18 +1,8 @@
 package com.example.lotus;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.lotus.databinding.ActivityProfileBinding;
 
 public class ProfileActivity extends AppCompatActivity {
 
